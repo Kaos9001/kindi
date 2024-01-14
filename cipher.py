6dda9f94365a7c81adb6771d4204745253768584
Matheus <matheus.castellob@gmail.com>
2024-01-14 15:19:10 -0300

    Melhorias no cipher.py

--- cipher.py
+++ cipher.py
@@ -1,138 +1,72 @@
-# Cifras encontradas em : 
-# - https://www.geeksforgeeks.org/substitution-cipher/
-# - https://www.geeksforgeeks.org/vigenere-cipher/
-import string
+alphabet_lower = "abcdefghijklmnopqrstuvwxyz"
+alphabet_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+def rot(ch, n):
+    if ch in alphabet_lower:
+        init_pos = 97
+    elif ch in alphabet_upper:
+        init_pos = 65
+    else:
+        return ch
+    pos = ord(ch) - init_pos + n
+    pos = pos % 26
+    nch = chr(pos+init_pos)
+
+    return nch
 
 def substitution(text, key, arg):
-    
-    # A list containing all characters
-    # all_letters= string.ascii_letters
-    lower_letters = string.ascii_lowercase
-    upper_letters = string.ascii_uppercase
-
-
-    dict_lower = {}
-    dict_upper = {}
-
     if isinstance(key, int):
-        for i in range(26):
-            if arg == "encode":
-                new_pos = (i+key)%26
-            elif arg == "decode":
-                new_pos = (i-key)%26
-            dict_lower[lower_letters[i]] = lower_letters[new_pos]
-            dict_upper[upper_letters[i]] = upper_letters[new_pos]
+        answer = ""
+        if arg == "encode":
+            encry = key
+        elif arg == "decode":
+            encry = - key
+        for i in range(len(text)):
+            answer += rot(text[i], encry)
+
+        return answer
     
-    else: # caso em que a key é do tipo subst
-        for i in range(len(lower_letters)):
-            if arg == "encode":
-                dict_lower[lower_letters[i]] = key[i]
-                dict_upper[upper_letters[i]] = key[i].upper()
-            elif arg == "decode":
-                dict_lower[key[i]] = lower_letters[i]
-                dict_upper[key[i].upper()] = upper_letters[i]
 
-
-
-    # este loop gera o nosso texto encriptado afetando apenas os caracteres 
-    # de letras romanas
-    cipher_txt=[]
-    for char in text:
-        if char in lower_letters:
-            temp = dict_lower[char]
-            cipher_txt.append(temp)
-        elif char in upper_letters:
-            temp = dict_upper[char]
-            cipher_txt.append(temp)
-        else:
-            temp =char
-            cipher_txt.append(temp)
-            
-    new_text= "".join(cipher_txt)
-    print(new_text)
-    return(new_text)
-
-    alphabet_lower = "abcdefghijklmnopqrstuvwxyz"
-    alphabet_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-
-
-
-# Python code to implement
-# Vigenere Cipher
- 
-     
-# This function returns the 
-# encrypted text generated 
-# with the help of the key
-def cipherText(text, key_o,arg):
-
-    # A list containing all characters
-    # all_letters= string.ascii_letters
-    lower_letters = string.ascii_lowercase
-    upper_letters = string.ascii_uppercase
-
-
-    # cria a chave para ter mesmo comprimento do texto
-    key = key_o.lower()
-    key = list(key)
-    if len(text) == len(key):
-        return(key)
-    elif len(text) > len(key):
-        for i in range(len(text) -
-                       len(key)):
-            key.append(key[i % len(key)])
-    else:
-        print("?????")
-    # key =  "" .join(key)
-
-    cipher_text = []
+def to_number_key(text, key):
+    lower_key = key.lower()
+    number_key = []
     for i in range(len(text)):
-        x = text[i]
-        # print(x)
-        if x in upper_letters:
-            x = x.lower()
-            if arg == "encode":
-                x = ord(x) + ord(key[i]) - 97 - 97
-            elif arg == "decode":
-                print("aaa")
-                x = ord(x) - ord(key[i])
-            x = x % 26 + ord("A")
-            # print("->{}".format(chr(x)))
-            cipher_text.append(chr(x).upper())
+        key_pos = i % len(lower_key)
+        num = ord(lower_key[key_pos]) - ord("a")
+        number_key.append(num)
+    return number_key
 
-        elif x in lower_letters:
-            if arg == "encode":
-                x = ord(x) + ord(key[i])
-            elif arg == "decode":
-                x = ord(x) - ord(key[i]) + 26
-            x = x % 26 + ord("a")
-            cipher_text.append(chr(x))
-        else: 
-            cipher_text.append(x)
+def vigenere(text, key, arg):
+    number_key = to_number_key(text, key)
+    answer = ""
+    for i in range(len(text)):
+        if arg == "encode":
+            encry = number_key[i]
+        elif arg == "decode":
+            encry = - number_key[i]
+        answer += rot(text[i], encry)
 
-    return("" . join(cipher_text))
-     
-# This function decrypts the 
-# encrypted text and returns 
-# the original text
-def originalText(cipher_text, key):
-    orig_text = []
-    for i in range(len(cipher_text)):
-        x = (ord(cipher_text[i]) -
-             ord(key[i])) % 26
-        x += ord('A')
-        orig_text.append(chr(x))
-    return("" . join(orig_text))
-     
+    return answer
 
-    
-# perm = "jvlxatqdmykhwnzbrgfieocspu"
+def autokey(text, key, arg):
+    if arg == "encode":
+        new_key = key + text
+        return vigenere(text, new_key, arg)
+    elif arg == "decode": 
+        number_key = to_number_key(key, key)
+        answer = ""
+        for i in range(len(text)):
+            current = rot(text[i], -number_key[0])
+            answer += current
+            number_key.append(to_number_key(current, current)[0])
+            number_key.pop(0)
+        return answer
 
-# encodada = substitution("isto e um teste \n ISTO E UM TESTE", perm, "encode")
-# decodada = substitution(encodada, perm, "decode")
 
-a = cipherText("GEEKSFORGEEKS", "AYUSH", "encode")
+
+
+a = autokey("attackatdawn", "QUEENLY", "encode")
 print(a)
-b = cipherText(a, "abc", "decode")
+b = autokey(a, "QUEENLY", "decode")
 print(b)
